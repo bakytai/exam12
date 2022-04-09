101bb6246e783afa63377240029cd783ff7f3e3d
Bakytai Egemberdieva <bakijw@gmail.com>
2022-04-09 12:21:35 +0600

    задала фикстурные данные

---
+++ api/fixtures.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+const User = require('./models/User');
+const Image = require('./models/Image');
+
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options);
+
+    const collections = await mongoose.connection.db.listCollections().toArray();
+
+    for (const coll of collections) {
+        await mongoose.connection.db.dropCollection(coll.name);
+    }
+
+    const [sara, john] = await User.create({
+        email: 'sara@gmail.com',
+        password: '000',
+        displayName: 'sara',
+        role: 'admin',
+        token: '5enDI2paOqusPavVWOnwB'
+    }, {
+        email: 'john@mail.ru',
+        password: '000',
+        displayName: 'john',
+        role: 'user',
+        token: '8enDI2paOqusBavVWOnwL'
+    });
+
+    await Image.create({
+        user: sara,
+        title: 'Ocean',
+        image: 'ocean.jpg'
+    }, {
+        user: sara,
+        title: 'Nature',
+        image: 'nature.jpg'
+    }, {
+        user: john,
+        title: 'My dog',
+        image: 's1.jpg'
+    }, {
+        user: john,
+        title: 'my hobby',
+        image: 'hobby.webp'
+    })
+
+    await mongoose.connection.close();
+};
+
+run().catch(e => console.log(e));

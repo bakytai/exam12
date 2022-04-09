4cfc0e3aacd76a9b0e498475f6e173fd1c61ff50
Bakytai Egemberdieva <bakijw@gmail.com>
2022-04-09 12:02:27 +0600

    Создала модель юзера, изображения. Прописала настройки базы

---
+++ api/config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    uploadPath: path.join(rootPath, 'public/uploads'),
+    mongo: {
+        db: 'mongodb://localhost/galleryApp',
+        options: {useNewUrlParser: true},
+    },
+    facebook: {
+        appId: '1030414684228398',
+        appSecret: 'eb6ff2b4bca355da3e20a351e59692a2'
+    }
+};
